1af5c147d2b4670f1f516a721256ec97b5b83642
dave2929 <55275304+dave2929@users.noreply.github.com>
2020-07-06 00:58:42 -0400

    PCA analysis

--- pca.py
+++ pca.py
@@ -1,25 +1,49 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 import pandas as pd 
-from sklearn.model_selection import train_test_split 
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 
 # Read combined.csv file
-dataset = pd.read_csv('combined.csv') 
+columns = ["videoCategoryId", "videoViews", "videoLikes", "videoDislikes", "videoCommentCount", "publishedZTimeFloat", "publishedDayOfWeek"]
+dataset = pd.read_csv('oldAndNewOnlyEnglish_noDuplicates.csv', usecols = columns) 
+videoLikes = dataset["videoLikes"]
+videoLikes.values[videoLikes > 10000] = 1
+videoLikes.values[videoLikes > 1] = 0
+# dataset.head()
+# print(len(videoLikes))
 
-# Split into training set and testing set
-dataset_train, dataset_test = train_test_split(dataset, test_size = 0.2, random_state = 0) 
+# Standardize data
+features = ["videoCategoryId", "videoViews", "publishedZTimeFloat", "publishedDayOfWeek"]
+x = dataset.loc[:, features].values
+y = dataset.loc[:,["videoLikes"]].values
+x = StandardScaler().fit_transform(x)
 
-# feature scaling
-from sklearn.preprocessing import StandardScaler 
-sc = StandardScaler() 
-dataset_train = sc.fit_transform(dataset_train) 
-dataset_test = sc.transform(dataset_test) 
+# Applying PCA 
+pca = PCA(n_components=2)
+principalComponents = pca.fit_transform(x)
+principalDf = pd.DataFrame(data = principalComponents
+             , columns = ['principal component 1', 'principal component 2'])
 
-# Applying PCA function on training and testing set 
-from sklearn.decomposition import PCA 
-pca = PCA(n_components = 2) 
-dataset_train = pca.fit_transform(dataset_train) 
-dataset_test = pca.transform(dataset_test) 
+finalDf = pd.concat([principalDf, dataset["videoLikes"]], axis = 1)
+finalDf.head(5)
 
-# fitting different method, then predict result
+# plot data
+fig = plt.figure(figsize = (8,8))
+ax = fig.add_subplot(1,1,1) 
+ax.set_xlabel('Principal Component 1', fontsize = 15)
+ax.set_ylabel('Principal Component 2', fontsize = 15)
+ax.set_title('2 Component PCA', fontsize = 20)
+
+targetLabel = ['High number of likes', 'Low number of likes']
+targets = [1, 0]
+colors = ['r', 'b']
+for target, color in zip(targets,colors):
+    indicesToKeep = finalDf["videoLikes"] == target
+    ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
+               , finalDf.loc[indicesToKeep, 'principal component 2']
+               , c = color
+               , s = 50)
+ax.legend(targetLabel)
+ax.grid()
